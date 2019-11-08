d055687bfaafea2650d4b81456518b461d682bdb
luoxin <2587447990@qq.com>
2019-11-08 21:25:40 +0800

    合并

--- Assets/Scripts/MenuFood/AddMenu.cs
+++ Assets/Scripts/MenuFood/AddMenu.cs
@@ -5,31 +5,18 @@ using UnityEngine.UI;
 
 public class AddMenu : MonoBehaviour {
     int count = 0;
-<<<<<<< HEAD:Assets/Scripts/AddMenu.cs
     RectTransform tran;
-=======
-    Transform tran;
-    
->>>>>>> 2d74089f283aaf40dd1a452178e8f5e6ed6791e2:Assets/Scripts/MenuFood/AddMenu.cs
+ 
     void Awake()
     {
         tran = GameObject.Find("MenuPoint").GetComponent<RectTransform>();
     }
     public void AddMenuPanel()
     {
-<<<<<<< HEAD:Assets/Scripts/AddMenu.cs
         if (count >= 6) return;
         //使用对象池生成菜单
         GameObject menu = ObjectPool.instance.CreateObject(GameConst.Menu);
         InitMenu(menu);
-=======
-       
-        if (count >= LevelInstance._instance.maxMenuNum) return;
-        GameObject menu= Resources.Load<GameObject>("Menu");
-        menu = Instantiate(menu,tran);
-        menu.transform.SetParent(tran);
-        menu.transform.localScale=new Vector3(1,1,1);
->>>>>>> 2d74089f283aaf40dd1a452178e8f5e6ed6791e2:Assets/Scripts/MenuFood/AddMenu.cs
         count += 1;
         
     }
