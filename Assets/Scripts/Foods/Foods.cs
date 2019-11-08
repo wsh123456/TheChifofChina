39c31697eae714798874b9bd1769bfdddb7905b4
wsh123456 <2321686159@qq.com>
2019-11-08 08:48:32 +0800

    json数据读取

---
+++ Assets/Scripts/Foods/Foods.cs
@@ -0,0 +1,20 @@
+﻿#region 模块信息
+// **********************************************************************
+// 作者(Author):王舜华
+// 版本号(Version):#VERSION#
+// Unity版本	(Unity Version):2017.4.20f
+// 创建时间(CreateTime):2019-11-7
+// 修改者列表(modifier):无
+// 模块描述(Module description):做好的食物
+// **********************************************************************
+#endregion
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Foods{
+    /// <summary>
+    /// 所需要用到的食材
+    /// </summary>
+    // public List<> usingFoodIList;
+}
---
+++ Assets/Scripts/Instance/LevelInstance.cs
@@ -0,0 +1,32 @@
+﻿#region 模块信息
+// **********************************************************************
+// 作者(Author):王舜华
+// 版本号(Version):#VERSION#
+// Unity版本	(Unity Version):2017.4.20f
+// 创建时间(CreateTime):2019-11-7
+// 修改者列表(modifier):无
+// 模块描述(Module description):关卡信息单例
+// **********************************************************************
+#endregion
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelInstance{
+    public float levelTime;
+
+    public static readonly LevelInstance _instance = new LevelInstance();
+    private LevelInstance(){}
+
+    public void LoadLevel(int level){
+        LevelModel message = JsonFileControl.LoadLevelMessage(1);
+        levelTime = message.levelTime;
+        Debug.Log("aaaa, " + levelTime + ", " + message.foodMenu.Count);
+        // 加载本关菜单
+        Debug.Log("adfwggsdffgdfsd" + LoadFoodIngredient(message.foodMenu));
+    }
+
+    public List<FoodModel> LoadFoodIngredient(List<string> foodMenu){
+        return JsonFileControl.LoadFoodIngredient(foodMenu);
+    }
+}
